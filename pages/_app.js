9d365c0fa91624da021f13fc974a38eb2018713a
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-01-04 12:31:14 +0100

    add solution

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import GlobalStyle from "../styles";
-
+import styled from "styled-components";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
     </>
   );
 }
+
+const Container = styled.main`
+  margin: 0 auto;
+  max-width: 600px;
+  padding: 20px;
+  display: grid;
+  min-height: 100vh;
+  place-content: center;
+  place-items: center;
+`;
---
+++ pages/volumes/the-two-towers/index.js
@@ -0,0 +1,37 @@
+import { volumes } from "../../../lib/data";
+import Link from "next/link";
+import Image from "next/image";
+import styled from "styled-components";
+
+const volume = volumes.find((volume) => volume.slug === "the-two-towers");
+
+export default function Page() {
+  return (
+    <Card>
+      <Link href="/volumes">Back to Overview</Link>
+      <h1>{volume.title}</h1>
+      <p>{volume.description}</p>
+      <ul>
+        {volume.books.map((book) => {
+          return (
+            <li key={book.ordinal}>
+              {book.ordinal}: {book.title}
+            </li>
+          );
+        })}
+      </ul>
+      <Image
+        src={volume.cover}
+        width={574}
+        height={931}
+        alt={`Cover of ${volume.title}`}
+      />
+    </Card>
+  );
+}
+
+const Card = styled.section`
+  color: ${volume.color};
+  padding: 20px;
+  border-radius: 14px;
+`;
